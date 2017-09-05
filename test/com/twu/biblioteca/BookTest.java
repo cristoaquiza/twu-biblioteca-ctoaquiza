758de0654e9ef8a05370ece215d30a2367ff9652
Cristian <cristoaquizal@gmail.com>
2017-09-05 11:11:38 -0500

    Book: Correct assert of book equals

--- test/com/twu/biblioteca/BookTest.java
+++ test/com/twu/biblioteca/BookTest.java
@@ -1,14 +1,19 @@
 package com.twu.biblioteca;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BookTest {
 
     @Test
-    public void testBookEqualsMethod() {
-        Book bookActual = new Book("MyBook2", "MyAuthor", 1900);
+    public void testBookEquals() {
+        Book bookActual = new Book("MyBook", "MyAuthor", 1900);
         Book bookExpected = new Book("MyBook", "MyAuthor", 1900);
-        assertEquals("failure - books do not have same fields", bookExpected, bookActual);
+        assertTrue("failure - books do not have same fields", bookActual.equals(bookExpected));
+    }
+
+    @Test
+    public void testBookPrint() {
+
     }
 }
